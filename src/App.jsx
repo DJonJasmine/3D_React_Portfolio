842dbc2228c2135bba8ce7f41400d2bd6b210b31
D'Jon Jasmine <harrisondjon1994@gmail.com>
2023-05-09 15:04:30 -0700

    renamed Works.jsx file to Work.jsx. Renamed all necessary code from works to work. Added code for application skeleton in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
-const App = () => {
-  return (
-    <div>
-      3D Developer Portfolio
-    </div>
-  )
-}
+// The layout/skeleton of application
 
-export default App
+// Import all the components
+// Router import
+import { BrowserRouter } from "react-router-dom";
+
+// Content components import
+import {
+	About,
+	Contact,
+	Experience,
+	Feedbacks,
+	Hero,
+	Navbar,
+	Tech,
+	Work,
+	StarsCanvas,
+} from "./components";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div className="relative z-0 bg-primary">
+				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+					<Navbar />
+					<Hero />
+				</div>
+
+				{/* Each component  is a section */}
+				<About />
+				<Experience />
+				<Tech />
+				<Work />
+				<Feedbacks />
+
+				{/* Used for star display */}
+				<div className="relative z-0">
+					<Contact />
+					<StarsCanvas />
+				</div>
+			</div>
+		</BrowserRouter>
+	);
+};
+
+export default App;
